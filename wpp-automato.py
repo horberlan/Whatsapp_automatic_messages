4ec77c5cbaf176fb9e6fa11dc21e99967efd7c56
horberlan <horberlan@gmail.com>
2021-01-07 20:57:42 -0300

    adding main file

---
+++ wpp-automato.py
@@ -0,0 +1,10 @@
+import pywhatkit
+ try: 
+    
+  # it will perform the Google search 
+  pywhatkit.search("Horberlan github") 
+  print("Searching...") 
+  
+except: 
+    # Printing Error Message 
+  print("An unknown error occured")
